17c8b2ab41e40a6bfa64a756528f378e913f48b3
Sergey Bakulin <vansergen@gmail.com>
2019-11-28 14:39:46 -0500

    feat(auth1): add getAccountInfo method

--- src/auth1.ts
+++ src/auth1.ts
@@ -1,6 +1,17 @@
 import { PublicClient1, PublicClient1Params } from "./public1";
 import { Signer } from "./signer";
 
+export type AccountInfo = [
+  {
+    leo_fee_disc_c2c: string;
+    leo_fee_disc_c2s: string;
+    leo_fee_disc_c2f: number;
+    maker_fees: string;
+    taker_fees: string;
+    fees: { pairs: string; maker_fees: string; taker_fees: string }[];
+  }
+];
+
 export type AuthenticatedClient1Options = PublicClient1Params & {
   key: string;
   secret: string;
@@ -16,10 +27,17 @@ export class AuthenticatedClient1 extends PublicClient1 {
     this.secret = secret;
   }
 
-  post({ body, uri }: { body: object; uri: string }): Promise<any> {
+  post({ body = {}, uri }: { body?: object; uri: string }): Promise<any> {
     body = { ...body, nonce: this.nonce, request: uri };
     const headers = Signer({ key: this.key, secret: this.secret, body });
     return super.post({ body, headers, uri });
+  }
+
+  /**
+   * Return information about your account (trading fees).
+   */
+  getAccountInfo(): Promise<AccountInfo> {
+    return this.post({ uri: "/v1/account_infos" });
   }
 
   get nonce(): string {
