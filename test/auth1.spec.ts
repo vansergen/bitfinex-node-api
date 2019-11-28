2ae6b5436474c984ec074fccfccf75f1518a541b
Sergey Bakulin <vansergen@gmail.com>
2019-11-28 14:46:18 -0500

    test(auth1): add getAccountInfo

--- test/auth1.spec.ts
+++ test/auth1.spec.ts
@@ -1,9 +1,11 @@
 import * as assert from "assert";
+import * as nock from "nock";
 import {
   AuthenticatedClient1,
   DefaultTimeout,
   DefaultSymbol,
-  DefaultCurrency
+  DefaultCurrency,
+  AccountInfo
 } from "../index";
 
 const key = "BitfinexAPIKey";
@@ -23,5 +25,40 @@ suite("AuthenticatedClient v1", () => {
     assert.deepStrictEqual(client.currency, DefaultCurrency);
     assert.deepStrictEqual(client.key, key);
     assert.deepStrictEqual(client.secret, secret);
+  });
+
+  test("getAccountInfo", async () => {
+    const response: AccountInfo = [
+      {
+        leo_fee_disc_c2c: "0.0",
+        leo_fee_disc_c2s: "0.0",
+        leo_fee_disc_c2f: 0,
+        maker_fees: "0.1",
+        taker_fees: "0.2",
+        fees: [
+          {
+            pairs: "BTC",
+            maker_fees: "0.1",
+            taker_fees: "0.2"
+          },
+          {
+            pairs: "LTC",
+            maker_fees: "0.1",
+            taker_fees: "0.2"
+          },
+          {
+            pairs: "ETH",
+            maker_fees: "0.1",
+            taker_fees: "0.2"
+          }
+        ]
+      }
+    ];
+    const uri = "/v1/account_infos";
+    nock(apiUri)
+      .post(uri, body => body.request === uri)
+      .reply(200, response);
+    const data = await client.getAccountInfo();
+    assert.deepStrictEqual(data, response);
   });
 });
