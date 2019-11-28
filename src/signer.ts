571b0ef71189ab62043617c46b57bb83850a4e67
Sergey Bakulin <vansergen@gmail.com>
2019-11-28 12:01:33 -0500

    feat: add signer function

---
+++ src/signer.ts
@@ -0,0 +1,25 @@
+import * as crypto from "crypto";
+
+export type SignerOptions = {
+  key: string;
+  secret: string;
+  body: object;
+};
+
+export type SignedHeaders = {
+  "X-BFX-APIKEY": string;
+  "X-BFX-PAYLOAD": string;
+  "X-BFX-SIGNATURE": string;
+};
+
+export function Signer({ key, secret, body }: SignerOptions): SignedHeaders {
+  const payload = Buffer.from(JSON.stringify(body)).toString("base64");
+  return {
+    "X-BFX-APIKEY": key,
+    "X-BFX-PAYLOAD": payload,
+    "X-BFX-SIGNATURE": crypto
+      .createHmac("sha384", secret)
+      .update(payload)
+      .digest("hex")
+  };
+}
