e533d15e920dee8dd78c35b684f587e4ab29ff9c
Sergey Bakulin <vansergen@gmail.com>
2020-12-24 18:59:55 +0300

    test: update `PublicClient1`

--- test/pubic1.spec.ts
+++ test/pubic1.spec.ts
@@ -4,7 +4,6 @@ import {
   PublicClient1,
   DefaultCurrency,
   DefaultSymbol,
-  DefaultTimeout,
   Ticker,
   Stats,
   FundingBook,
@@ -19,25 +18,16 @@ const apiUri = "https://api.bitfinex.com";
 
 suite("PublicClient v1", () => {
   test("constructor", () => {
-    assert.deepStrictEqual(client._rpoptions, {
-      baseUrl: apiUri,
-      timeout: DefaultTimeout,
-      json: true,
-    });
     assert.deepStrictEqual(client.symbol, DefaultSymbol);
     assert.deepStrictEqual(client.currency, DefaultCurrency);
   });
 
-  test("constructor (with options)", () => {
+  test("constructor (with `currency`)", () => {
     const symbol = "ethbtc";
-    const timeout = 20000;
-    const client = new PublicClient1({ symbol, timeout });
-    assert.deepStrictEqual(client._rpoptions, {
-      baseUrl: apiUri,
-      timeout: timeout,
-      json: true,
-    });
-    assert.deepStrictEqual(client.symbol, symbol);
+    const currency = "EUR";
+    const otherClient = new PublicClient1({ symbol, currency });
+    assert.deepStrictEqual(otherClient.symbol, symbol);
+    assert.deepStrictEqual(otherClient.currency, currency);
   });
 
   test(".getTicker()", async () => {
@@ -52,10 +42,24 @@ suite("PublicClient v1", () => {
       volume: "7842.11542563",
       timestamp: "1444253422.348340958",
     };
-    nock(apiUri)
-      .get("/v1/pubticker/" + symbol)
-      .reply(200, response);
+    nock(apiUri).get(`/v1/pubticker/${symbol}`).reply(200, response);
     const data = await client.getTicker({ symbol });
+    assert.deepStrictEqual(data, response);
+  });
+
+  test(".getTicker() (with no `symbol`)", async () => {
+    const response: Ticker = {
+      mid: "244.755",
+      bid: "244.75",
+      ask: "244.76",
+      last_price: "244.82",
+      low: "244.2",
+      high: "248.19",
+      volume: "7842.11542563",
+      timestamp: "1444253422.348340958",
+    };
+    nock(apiUri).get(`/v1/pubticker/${DefaultSymbol}`).reply(200, response);
+    const data = await client.getTicker();
     assert.deepStrictEqual(data, response);
   });
 
@@ -75,17 +79,35 @@ suite("PublicClient v1", () => {
         volume: "275148.09653645",
       },
     ];
-    nock(apiUri)
-      .get("/v1/stats/" + symbol)
-      .reply(200, response);
+    nock(apiUri).get(`/v1/stats/${symbol}`).reply(200, response);
     const data = await client.getStats({ symbol });
+    assert.deepStrictEqual(data, response);
+  });
+
+  test(".getStats() (with no `symbol`)", async () => {
+    const response: Stats = [
+      {
+        period: 1,
+        volume: "7967.96766158",
+      },
+      {
+        period: 7,
+        volume: "55938.67260266",
+      },
+      {
+        period: 30,
+        volume: "275148.09653645",
+      },
+    ];
+    nock(apiUri).get(`/v1/stats/${DefaultSymbol}`).reply(200, response);
+    const data = await client.getStats();
     assert.deepStrictEqual(data, response);
   });
 
   test(".getFundingBook()", async () => {
     const currency = "usd";
     const limit_bids = 1;
-    const limit_asks = 1;
+    let limit_asks: undefined;
     const response: FundingBook = {
       bids: [
         {
@@ -107,14 +129,40 @@ suite("PublicClient v1", () => {
       ],
     };
     nock(apiUri)
-      .get("/v1/lendbook/" + currency)
-      .query({ limit_bids, limit_asks })
+      .get(`/v1/lendbook/${currency}`)
+      .query({ limit_bids })
       .reply(200, response);
     const data = await client.getFundingBook({
       currency,
       limit_bids,
       limit_asks,
     });
+    assert.deepStrictEqual(data, response);
+  });
+
+  test(".getFundingBook() (with no `currency`)", async () => {
+    const response: FundingBook = {
+      bids: [
+        {
+          rate: "9.1287",
+          amount: "5000.0",
+          period: 30,
+          timestamp: "1444257541.0",
+          frr: "No",
+        },
+      ],
+      asks: [
+        {
+          rate: "8.3695",
+          amount: "407.5",
+          period: 2,
+          timestamp: "1444260343.0",
+          frr: "No",
+        },
+      ],
+    };
+    nock(apiUri).get(`/v1/lendbook/${DefaultCurrency}`).reply(200, response);
+    const data = await client.getFundingBook();
     assert.deepStrictEqual(data, response);
   });
 
@@ -140,7 +188,7 @@ suite("PublicClient v1", () => {
       ],
     };
     nock(apiUri)
-      .get("/v1/book/" + symbol)
+      .get(`/v1/book/${symbol}`)
       .query({ limit_bids, limit_asks, group })
       .reply(200, response);
     const data = await client.getOrderBook({
@@ -149,6 +197,28 @@ suite("PublicClient v1", () => {
       limit_asks,
       group,
     });
+    assert.deepStrictEqual(data, response);
+  });
+
+  test(".getOrderBook() (with no `symbol`)", async () => {
+    const response: OrderBook = {
+      bids: [
+        {
+          price: "574.61",
+          amount: "0.1439327",
+          timestamp: "1472506127.0",
+        },
+      ],
+      asks: [
+        {
+          price: "574.62",
+          amount: "19.1334",
+          timestamp: "1472506126.0",
+        },
+      ],
+    };
+    nock(apiUri).get(`/v1/book/${DefaultSymbol}`).reply(200, response);
+    const data = await client.getOrderBook();
     assert.deepStrictEqual(data, response);
   });
 
@@ -167,7 +237,7 @@ suite("PublicClient v1", () => {
       },
     ];
     nock(apiUri)
-      .get("/v1/trades/" + symbol)
+      .get(`/v1/trades/${symbol}`)
       .query({ limit_trades, timestamp })
       .reply(200, response);
     const data = await client.getTrades({
@@ -175,6 +245,22 @@ suite("PublicClient v1", () => {
       timestamp,
       limit_trades,
     });
+    assert.deepStrictEqual(data, response);
+  });
+
+  test(".getTrades() (with no `symbol`)", async () => {
+    const response: Trade[] = [
+      {
+        timestamp: 1444266681,
+        tid: 11988919,
+        price: "244.8",
+        amount: "0.03297384",
+        exchange: "bitfinex",
+        type: "sell",
+      },
+    ];
+    nock(apiUri).get(`/v1/trades/${DefaultSymbol}`).reply(200, response);
+    const data = await client.getTrades();
     assert.deepStrictEqual(data, response);
   });
 
@@ -191,7 +277,7 @@ suite("PublicClient v1", () => {
       },
     ];
     nock(apiUri)
-      .get("/v1/lends/" + currency)
+      .get(`/v1/lends/${currency}`)
       .query({ timestamp, limit_lends })
       .reply(200, response);
     const data = await client.getLends({
@@ -199,6 +285,20 @@ suite("PublicClient v1", () => {
       timestamp,
       limit_lends,
     });
+    assert.deepStrictEqual(data, response);
+  });
+
+  test(".getLends() (with no `currency`)", async () => {
+    const response: Lend[] = [
+      {
+        rate: "9.8998",
+        amount_lent: "22528933.77950878",
+        amount_used: "0.0",
+        timestamp: 1444264307,
+      },
+    ];
+    nock(apiUri).get(`/v1/lends/${DefaultCurrency}`).reply(200, response);
+    const data = await client.getLends();
     assert.deepStrictEqual(data, response);
   });
 
